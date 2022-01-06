a0cb0f95292fee20770182a4b02c6a5df84fadbb
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-06 22:53:11 +0300

    Create simple http server.

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
-package mai
+package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+// Define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home (w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world."))
+}
+
 func main () {
-	fmt.Println("Hello, world.")
+
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servemux we just created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit. Note
+	// that any error returned by http.ListenAndServe() is always non-nil.
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)	
 }
