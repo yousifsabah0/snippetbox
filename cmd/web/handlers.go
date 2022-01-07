994de469cf673cd4383a8c549c92e296dad1674a
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-07 14:59:35 +0300

    More with html ..

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,7 +15,12 @@ func home (w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/home.page.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Fatal(err.Error())
 		http.Error(w, "Internal server error.", http.StatusInternalServerError)
