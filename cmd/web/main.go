2f22170873cbe67828208924546d313273002465
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-07 18:05:18 +0300

    Add custom logging.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main () {
+	// Get command line flags
+	addr := flag.String("addr", ":8080", "HTTP network address.")
+	flag.Parse()
+
+	// Create custom loggers
+	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize new servemux.
 	mux := http.NewServeMux()
 
 	// Handle routes
@@ -17,6 +28,7 @@ func main () {
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err)
+	infoLogger.Printf("Server has been lift off at %v", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLogger.Fatal(err)
 }
