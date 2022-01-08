f3c53aa70627f66453c6c440f60f22a00c51c360
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-08 22:20:41 +0300

    Change routes and use middleware's

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
 )
 
-func (app *Application) routes () *http.ServeMux {
-	// Initialize new servemux.
-	mux := http.NewServeMux()
+func (app *Application) routes () http.Handler {
+	// // Initialize new servemux.
+	// mux := http.NewServeMux()
 
-	// Handle routes
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippets", app.showSnippet)
-	mux.HandleFunc("/snippets/new", app.createSnippet)
+	// // Handle routes
+	// mux.HandleFunc("/", app.home)
+	// mux.HandleFunc("/snippets", app.showSnippet)
+	// mux.HandleFunc("/snippets/new", app.createSnippet)
 
-	// Handle static files
+	// // Handle static files
+	// fileserver := http.FileServer(http.Dir("./ui/static/"))
+	// mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	// return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+
+	middlewares := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(app.home))
+	// mux.Get("/snippets/new", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippets/new", http.HandlerFunc(app.createSnippet))
+	
+	mux.Get("/snippets/:id", http.HandlerFunc(app.showSnippet))
+
+		// Handle static files
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	mux.Get("/static/", http.StripPrefix("/static", fileserver))
 
-	return mux
+	return middlewares.Then(mux)
 }
