a22bea8f0bd784293300544adf009275f6ed62b6
Yousif Sabah <iyousifsabah@gmail.com>
2022-01-08 17:27:11 +0300

    Add mysql pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Application struct {
@@ -15,11 +18,20 @@ type Application struct {
 func main () {
 	// Get command line flags
 	addr := flag.String("addr", ":8080", "HTTP network address.")
+	dns := flag.String("dns", "stark:1538@/snippetbox?parserTime=true", "Mysql connection string.")
 	flag.Parse()
 
 	// Create custom loggers
 	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize db connection
+	db, err := openDB(*dns)
+	if err != nil {
+		errorLogger.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// Initialize new instance of Application struct
 	app := &Application{
@@ -36,6 +48,10 @@ func main () {
 	}
 
 	infoLogger.Printf("Server has been lift off at %v", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLogger.Fatal(err)
+}
+
+func openDB (dns string) (*sql.DB, error) {
+	return sql.Open("mysql", dns)
 }
